5a6c87e1e68feda2f7ee34c03338229829a12cc8
azu <azuciao@gmail.com>
2013-02-21 17:41:35 +0900

    Initial Commit

---
+++ AZPodfile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AZPodfile
+//
+//  Created by azu on 2013/02/21.
+//  Copyright (c) 2013年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AZPodfile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AZPodfile
+//
+//  Created by azu on 2013/02/21.
+//  Copyright (c) 2013年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
